8bbc6b024f75eba70ac1fe9f97a71fb24e03f62d
frederycksales <141734888+frederycksales@users.noreply.github.com>
2023-09-01 17:07:34 -0300

    Melhoria da Navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,4 +1,8 @@
-import React, { useState } from "react";
+import React, { useState, useContext } from "react";
+import { useMediaQuery, useTheme } from "@mui/material";
+import { useDispatch } from "react-redux";
+import { setMode } from "../state";
+import FlexBetween from "./FlexBetween";
 import {
   LightModeOutlined,
   DarkModeOutlined,
@@ -7,9 +11,6 @@ import {
   NotificationsNoneOutlined,
   PersonOutlined,
 } from "@mui/icons-material";
-import FlexBetween from "./FlexBetween";
-import { useDispatch } from "react-redux";
-import { setMode } from "../state";
 import {
   AppBar,
   IconButton,
@@ -17,15 +18,18 @@ import {
   Toolbar,
   Menu,
   MenuItem,
-  useTheme,
   Tooltip,
 } from "@mui/material";
+
+const LocaleContext = React.createContext('en');
 
 const Navbar = ({ isSidebarOpen, setIsSidebarOpen }) => {
   const [searchQuery, setSearchValue] = useState("");
   const [anchorEl, setAnchorEl] = useState(null);
   const dispatch = useDispatch();
   const theme = useTheme();
+  const isMobile = useMediaQuery(theme.breakpoints.down('sm'));
+  const locale = useContext(LocaleContext);
 
   const handleProfileIcoOpen = (event) => {
     setAnchorEl(event.currentTarget);
@@ -37,7 +41,19 @@ const Navbar = ({ isSidebarOpen, setIsSidebarOpen }) => {
 
   const handleSearch = () => {
     console.log("Searching for:", searchQuery);
-  }
+  };
+
+  const handleProfile = () => {
+    console.log("Navigating to Profile");
+  };
+
+  const handleSettings = () => {
+    console.log("Navigating to Settings");
+  };
+
+  const handleLogout = () => {
+    console.log("Logging out");
+  };
 
   return (
     <AppBar
@@ -55,51 +71,53 @@ const Navbar = ({ isSidebarOpen, setIsSidebarOpen }) => {
           '&.MuiToolbar-root': {
             minHeight: "50px",
           }
-        }}>
+        }}
+      >
 
-        {/* Esquerda */}
-        <FlexBetween>
-          <Tooltip title="Menu">
-            <IconButton onClick={() => setIsSidebarOpen(!isSidebarOpen)}>
-              <MenuIcon sx={{ color: theme.palette.primary.main, }} />
-            </IconButton>
-          </Tooltip>
-          <FlexBetween
-            backgroundColor={theme.palette.primary.light}
-            borderRadius="9px"
-            gap="3rem"
-            p="0.1rem 1.5rem"
-            height="25px"
-          >
-            <InputBase
-              value={searchQuery}
-              onChange={(e) => setSearchValue(e.target.value)}
-              onKeyDown={(e) => {
-                if (e.key === "Enter") {
-                  handleSearch();
-                }
-              }}
-              sx={{
-                '& .MuiInputBase-input': {
-                  '&::placeholder': {
-                    color: theme.palette.text.secondary,
-                    fontWeight: 'bold',
-                    fontFamily: ["Poppins", "sans-serif"].join(","),
-                  }
-                }
-              }}
-              placeholder="Pesquisar..." />
-            <Tooltip title="Pesquisar">
-              <IconButton onClick={() => handleSearch()}>
-                <Search sx={{ color: theme.palette.text.secondary }} />
+        {!isMobile && (
+          <FlexBetween>
+            <Tooltip title="Menu">
+              <IconButton onClick={() => setIsSidebarOpen(!isSidebarOpen)}>
+                <MenuIcon sx={{ color: theme.palette.primary.main }} />
               </IconButton>
             </Tooltip>
+            <FlexBetween
+              backgroundColor={theme.palette.primary.light}
+              borderRadius="9px"
+              gap="3rem"
+              p="0.1rem 1.5rem"
+              height="25px"
+            >
+              <InputBase
+                value={searchQuery}
+                onChange={(e) => setSearchValue(e.target.value)}
+                onKeyDown={(e) => {
+                  if (e.key === "Enter") {
+                    handleSearch();
+                  }
+                }}
+                sx={{
+                  '& .MuiInputBase-input': {
+                    '&::placeholder': {
+                      color: theme.palette.text.secondary,
+                      fontWeight: 'bold',
+                      fontFamily: ["Poppins", "sans-serif"].join(","),
+                    }
+                  }
+                }}
+                placeholder={locale === 'pt' ? 'Pesquisar...' : 'Search...'}
+              />
+              <Tooltip title={locale === 'pt' ? 'Pesquisar' : 'Search'}>
+                <IconButton onClick={handleSearch}>
+                  <Search sx={{ color: theme.palette.text.secondary }} />
+                </IconButton>
+              </Tooltip>
+            </FlexBetween>
           </FlexBetween>
-        </FlexBetween>
+        )}
 
-        {/*Direita*/}
         <FlexBetween gap="0.1rem">
-          <Tooltip title="Mudar Tema">
+          <Tooltip title={locale === 'pt' ? 'Mudar Tema' : 'Switch Theme'}>
             <IconButton onClick={() => dispatch(setMode())}>
               {theme.palette.mode === "dark" ? (
                 <DarkModeOutlined sx={{ color: theme.palette.primary.main, fontSize: "20px" }} />
@@ -108,12 +126,12 @@ const Navbar = ({ isSidebarOpen, setIsSidebarOpen }) => {
               )}
             </IconButton>
           </Tooltip>
-          <Tooltip title="Notificações">
+          <Tooltip title={locale === 'pt' ? 'Notificações' : 'Notifications'}>
             <IconButton>
               <NotificationsNoneOutlined sx={{ color: theme.palette.primary.main, fontSize: "20px" }} />
             </IconButton>
           </Tooltip>
-          <Tooltip title="Perfil">
+          <Tooltip title={locale === 'pt' ? 'Perfil' : 'Profile'}>
             <IconButton onClick={handleProfileIcoOpen}>
               <PersonOutlined sx={{ color: theme.palette.primary.main, fontSize: "20px" }} />
             </IconButton>
@@ -131,9 +149,15 @@ const Navbar = ({ isSidebarOpen, setIsSidebarOpen }) => {
               }
             }}
           >
-            <MenuItem onClick={handleProfileIcoClose}>Profile</MenuItem>
-            <MenuItem onClick={handleProfileIcoClose}>Settings</MenuItem>
-            <MenuItem onClick={handleProfileIcoClose}>Logout</MenuItem>
+            <MenuItem onClick={handleProfile}>
+              {locale === 'pt' ? 'Perfil' : 'Profile'}
+            </MenuItem>
+            <MenuItem onClick={handleSettings}>
+              {locale === 'pt' ? 'Configurações' : 'Settings'}
+            </MenuItem>
+            <MenuItem onClick={handleLogout}>
+              {locale === 'pt' ? 'Sair' : 'Logout'}
+            </MenuItem>
           </Menu>
 
         </FlexBetween>
