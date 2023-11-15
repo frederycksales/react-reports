d57afe49f67d72be0ecd81f5ad92e51a93675110
Jhon Cruz <jhonskaz@gmail.com>
2023-11-15 18:32:35 -0300

    Tratando erros nas rotas CRUD dos reports

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -9,12 +9,13 @@ import {
   deleteRequest,
 } from "./controllers/ReportsController.js";
 import { registerUser, login } from "./controllers/UsersController.js";
+import { verifyToken } from "./middleware/auth.js";
 const routes = Router();
 
 // Rotas reports
 routes.post("/report", createReport);
 routes.post("/report/:id", updateReport);
-routes.get("/getAllReports", getAllReports);
+routes.get("/getAllReports", verifyToken, getAllReports);
 routes.get("/getAllOpenedReports", getAllOpenedReports);
 routes.get("/getAllClosedReports", getAllClosedReports);
 routes.delete("/report/:id", deleteRequest);
